f96aef3e7426a1e0bdcf6ed6b96466346cccf47a
Huaxing YUAN <37816968+huaxing-yuan@users.noreply.github.com>
2024-08-30 16:06:25 +0200

    [Mobile] force an exception if appium session is not opened (#74)
    * Accessibility: update to axe core 4.9
    * Accessibility: French verison of report
    * add support of some language for accessibility test.
    * update unit test for multi language.

--- src/AxaFrance.AxeExtended.HtmlReport/OverallReportBuilder.cs
+++ src/AxaFrance.AxeExtended.HtmlReport/OverallReportBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using Deque.AxeCore.Commons;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -15,6 +16,7 @@ namespace AxaFrance.AxeExtended.HtmlReport
     public class OverallReportBuilder
     {
         private PageReportOptions options;
+        internal JObject locale = null;
 
         /// <summary>
         /// Initialize OverallReportBuilder with default options.
@@ -84,8 +86,8 @@ namespace AxaFrance.AxeExtended.HtmlReport
             int sequence = 1;
 
             //export the overallpage
-            var html = PageReportBuilder.GetRessource("overall-result.html");
-            var rowTemplate = PageReportBuilder.GetRessource("overall-tablerow.html");
+            var html = PageReportBuilder.GetRessource("overall-result.html").ToLocale(options.ReportLanguage);
+            var rowTemplate = PageReportBuilder.GetRessource("overall-tablerow.html").ToLocale(options.ReportLanguage);
             var TableRowPageResult = new StringBuilder();
             
             //export each subpages and generation of 
